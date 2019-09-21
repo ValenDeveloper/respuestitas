79dcb4e8ab1fe550ce1d3598f1d12efbc41af3eb
Edmundo Gonzalez Hernandez <mundo2599@outlook.com>
2019-09-20 23:04:15 -0500

    Formatter respuestitaspro.java

--- app/src/Valen/respuestitaspro.java
+++ app/src/Valen/respuestitaspro.java
@@ -7,43 +7,43 @@ public class respuestitaspro {
 
         System.out.println("Hola, aquí podras añadir los mensajes y las respuestas deseadas");
 
-        ArrayList <String> condi= new ArrayList <String>();
-        ArrayList <String> respt= new ArrayList <String>();
+        ArrayList<String> condi = new ArrayList<String>();
+        ArrayList<String> respt = new ArrayList<String>();
         Scanner recb;
         String imput;
-        String yn= "";
+        String yn = "";
 
-        while(!yn.equals("n\r")) {
+        while (!yn.equals("n\r")) {
 
             System.out.println("Escribe el mensaje recibido");
 
-            recb= new Scanner(System.in).useDelimiter("\n");
+            recb = new Scanner(System.in).useDelimiter("\n");
 
-            imput= recb.next();
-           
+            imput = recb.next();
+
             condi.add(imput);
 
             System.out.println("Ahora escriba lo que desea responder");
 
-            recb= new Scanner(System.in).useDelimiter("\n");
+            recb = new Scanner(System.in).useDelimiter("\n");
 
-            imput= recb.next();
+            imput = recb.next();
 
             respt.add(imput);
 
             System.out.println("¿Desea añadir mas respuestas? (Y/N)");
 
-            recb= new Scanner(System.in).useDelimiter("\n");
-            
-            yn= recb.next();
+            recb = new Scanner(System.in).useDelimiter("\n");
+
+            yn = recb.next();
 
         }
 
         System.out.println("Introduce tu mensaje");
 
-        Scanner mss= new Scanner(System.in).useDelimiter("\n");
+        Scanner mss = new Scanner(System.in).useDelimiter("\n");
 
-        msj msj1= new msj(mss.next(), condi, respt);
+        msj msj1 = new msj(mss.next(), condi, respt);
 
         System.out.println(msj1.contestacion());
     }
@@ -51,16 +51,16 @@ public class respuestitaspro {
 
 class msj {
 
-    public msj(String mensj, ArrayList <String> condi, ArrayList <String> respt) {
+    public msj(String mensj, ArrayList<String> condi, ArrayList<String> respt) {
 
-        mensaje= mensj;
+        mensaje = mensj;
 
-        for(int i=0; i<condi.size(); i++) {
+        for (int i = 0; i < condi.size(); i++) {
 
-            if(mensaje.contains(condi.get(i))) {
+            if (mensaje.contains(condi.get(i))) {
 
-                respuesta+= respt.get(i);
-                
+                respuesta += respt.get(i);
+
             }
 
         }
@@ -73,6 +73,6 @@ class msj {
     }
 
     private String mensaje;
-    
-    private String respuesta="";
+
+    private String respuesta = "";
 }
